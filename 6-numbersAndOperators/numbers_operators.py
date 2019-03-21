f562a98c00b1a3d7dc92fb989066f8cbbfbb5f23
Katie Duane <katiejduane@gmail.com>
2019-03-21 13:25:33 -0400

    nums and operators

---
+++ 6-numbersAndOperators/numbers_operators.py
@@ -0,0 +1,34 @@
+# integer = 4 (pos or neg)
+# float = 4.2 (or however many decimal points) (also called 'floating points')(pos or neg)
+print(type(9))
+print(type(9.609))
+
+print(1 + 1.0)
+# if any part of the expression is a float, the result is a float
+
+# OPERATORS:
+# + addition
+# - subtraction
+# * multiplication
+# / division
+# ** exponation
+# % modulo
+# // integer division
+
+print(1/2)
+print(1/3)
+
+# PEMDAS applies to python!
+
+# WEIRDER OPERATORS!
+# Exponation: 
+print(2 ** 3)
+
+# Modulo (the 'remainder' operator)
+print(4%2) #remainder 0
+print(10% 3) #remainder 1
+print(111 % 7)
+
+# Integer Division:
+print(10//3)
+# it does the division but gives only an INT, not a float, even if a float is the "correct" answer
