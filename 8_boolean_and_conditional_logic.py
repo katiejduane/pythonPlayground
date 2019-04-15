c8095475659cdee9b6a9251c36e44e09926117cd
Katie Duane <katiejduane@gmail.com>
2019-04-15 17:58:14 -0400

    boolean logic

--- 8_boolean_and_conditional_logic.py
+++ 8_boolean_and_conditional_logic.py
@@ -54,3 +54,28 @@ if not weekend:
 # is compares where it's stored in memory
 
 
+# bouncer.py
+age = input("How old are you: ")
+if age:
+	age = int(age)
+	if age >= 18 and age < 21:
+		print("You can enter, but need a wristband!")
+	elif age >= 21:
+	    print("You are good to enter and can drink!")
+	else:
+		print("You can't come in, little one! :(")
+else:
+	print("Please enter an age!")
+
+# VERSION 2 with slightly refactored conditional logic
+age = input("How old are you: ")
+if age:
+	age = int(age)
+	if age >= 21:
+	    print("You are good to enter and can drink!")
+	elif age >= 18:
+	    print("You can enter, but need a wristband!")
+	else:
+		print("You can't come in, little one! :(")
+else:
+	print("Please enter an age!")
