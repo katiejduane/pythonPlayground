1a2cabc5475717034151720d77ccb23659bea910
Katie Duane <katiejduane@gmail.com>
2019-04-16 22:28:34 -0400

    list comp basics and function basics

---
+++ 13_list_comprehension.py
@@ -0,0 +1,22 @@
+# list comprehension is a powerful way to make new lists! copies of lists, etc...
+
+# syntax
+# [___ for ___ in ___]
+
+nums = [1,2,3]
+new_nums = [x*10 for x in nums]
+print(new_nums)
+
+# it can also be done with a loop, but this is more concise!
+
+name = 'colt'
+name_list = [char.upper() for char in name]
+print(name_list)
+
+friends = ['sean', 'jason', 'greg']
+[friend[0].upper() for friend in friends] # this isn't working for me!
+print(friends)
+
+more_nums = [1,2,3,4,5]
+string_nums = [str(num) for num in more_nums]
+print(string_nums)
